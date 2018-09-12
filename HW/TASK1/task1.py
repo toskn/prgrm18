f00ee39641e7e8bba7d31b7af9c94ee7282cfb70
toskn <300899@gmail.com>
2018-09-12 15:01:54 +0300

    uploading an upgraded part of the 1 task

--- HW/TASK1/task1.py
+++ HW/TASK1/task1.py
@@ -1,12 +1,12 @@
 # It's supposed in this program that every letter should be guessed,
 # which means that if there is a repeated letter in the word,
 # it should be guessed twice.
+# "ё" character isn't used
 import random
+import re
 
 
 # This  def allows player to choose the category
-
-
 def choose_category():
     players_choice = int(input('Type the number of the category you want to play: \n 1) Столярные инструменты \n '
                                '2) Слесарные инструменты \n 3) Футбольные клубы, образованные в 1933 году'))
@@ -14,8 +14,6 @@ def choose_category():
 
 
 # This def opens the chosen file to a list
-
-
 def open_file_to_list(players_choice):
     # Players' input is valid
 
@@ -40,56 +38,73 @@ def open_file_to_list(players_choice):
 
 
 # This def selects a random word from the list
-
-
 def random_word(file):
     i = random.randint(0, 11)
     word_to_guess = file[i]
     return word_to_guess
 
 
-# This def creates a variable which is actually the random word from previous def^ but all the letters are '_' now
-
-
-def make_word_hidden(word_to_guess):
-    i = 0
-    underline = ''
-    while i < len(word_to_guess):
-        print('_ ')
-        underline += '_ '
-        i += 1
-    print('У вас есть 10 попыток, чтобы угадать слово из' + str(len(word_to_guess)) + 'букв')
-    return underline
+# This def creates a variable which is actually the random word from previous def, but all the letters are '_' now
+# its commented now, since using this def separately makes the program do the job twice in [def game(…, …)]
+# def make_word_hidden(word_to_guess):
+    #     i = 0
+    #     underline = ''
+    #     while i < len(word_to_guess):
+    #        print('_ ')
+    #        underline += '_ '
+    #        i += 1
+    #    print('У вас есть 10 попыток, чтобы угадать слово из' + str(len(word_to_guess)) + 'букв')
+#    return underline
 
 
 # This def is the gameplay
-
-
-def game(word_to_guess, underline):
+def game(word_to_guess):
     attempts = 10
+    i = 0
     list_of_letters = list(word_to_guess)
-    while attempts > 0:
+    underline = ''
+
+    while i < len(word_to_guess):
+        underline += '_ '
+        i += 1
+    print(underline)
+    print('У вас есть 10 попыток, чтобы угадать слово из ' + str(len(word_to_guess)) + ' букв')
+
+    # Preparation finished, the game starts here.
+    while attempts > 0 and underline.split(' ') != list_of_letters:
         players_guess = str(input('Guess a letter'))
 
-        # Player did guess the letter: a letter of the hidden word is revealed.
+        # Here the players' input is checked:
+        # 1) if it contain letters
+        # 2) if there is only one letter
+        # 3) if letter is cyrillic
+        if players_guess.isalpha() and len(players_guess) == 1 and bool(re.search('[а-яА-Я]', players_guess)):
 
-        for i in range(len(list_of_letters)):
-            if players_guess == list_of_letters[i]:
-                list_underline = list(underline)
-                list_underline[i] = players_guess + ' '
-                if underline.split(' ') == list_of_letters:
-                    print('WIN')
+            # Player did guess the letter; a letter of the hidden word is revealed.
+            for i in range(len(list_of_letters)):
+                if players_guess == list_of_letters[i]:
+                    list_underline = list(underline)
+                    list_underline[i] = players_guess + ' '
+                    if underline.split(' ') == list_of_letters:
+                        print('WIN')
 
-            # Player didn't guess the letter
+                # Player didn't guess the letter
+                # For the sake of optimization its better to reverse the order of elifs,
+                # but here it doesnt really matter
+                else:
+                    attempts -= 1
+                    if attempts == 0:
+                        print('Попыток не осталось! Вы проиграли. Хотите сыграть еще раз?')
+                    elif attempts == 1:
+                        print('Осталась 1 попытка')
+                    elif 1 < attempts < 5:
+                        print('Осталось ' + str(attempts) + ' попытки')
+                    elif 5 <= attempts <= 10:
+                        print('Осталось ' + str(attempts) + ' попыток')
 
-            else:
-                attempts -= 1
-                if attempts == 0:
-                    print('Попыток не осталось! Вы проиграли. Хотите сыграть еще раз?')
-                elif attempts == 1:
-                    print('Осталась 1 попытка')
-                elif 1 < attempts < 5:
-                    print('Осталось' + attempts + 'попытки')
-                elif 5 <= attempts <= 10:
-                    print('Осталось' + attempts + 'попыток')
-        print(underline)
+            print(underline)
+        else:
+            print("That's not a valid input")
+
+
+game(random_word(open_file_to_list(choose_category())))
