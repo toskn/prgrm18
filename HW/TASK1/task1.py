5772198b5699b0298fd836ade29c2e1f2dd18236
toskn <300899@gmail.com>
2018-09-13 15:15:34 +0300

    almost there with the 1st task

--- HW/TASK1/task1.py
+++ HW/TASK1/task1.py
@@ -1,6 +1,3 @@
-# It's supposed in this program that every letter should be guessed,
-# which means that if there is a repeated letter in the word,
-# it should be guessed twice.
 # "ё" character isn't used
 # i, ii, iii are used as counters
 import random
@@ -10,24 +7,23 @@ import sys
 
 # This  def allows player to choose the category
 def choose_category():
-    players_choice = int(input('Введите номер категории, которую хотите играть: \n 1) Столярные инструменты \n '
-                               '2) Слесарные инструменты \n 3) Футбольные клубы, образованные в 1933 году'))
+    players_choice = input('Введите номер категории, которую хотите играть: \n 1) Столярные инструменты \n '
+                           '2) Слесарные инструменты \n 3) Футбольные клубы, образованные в 1933 году')
     return players_choice
 
 
 # This def opens the chosen file to a list
 def open_file_to_list(players_choice):
-
     # Players' input is valid
-    if players_choice == 1:
+    if players_choice == '1':
         filename = 'stolyarnie_instrumenti.txt'
-    elif players_choice == 2:
+    elif players_choice == '2':
         filename = 'slesarnie_instrumenti.txt'
-    elif players_choice == 3:
+    elif players_choice == '3':
         filename = 'football_clubs_origin_1933.txt'
 
     # Players' input is invalid
-    elif players_choice != 1 and players_choice != 2 and players_choice != 3:
+    elif players_choice != '1' and players_choice != '2' and players_choice != '3':
         sys.exit("Такая категория отсутствует. Перезапустите игру.")
 
     # Chosen .txt file is opened as a list
@@ -45,21 +41,23 @@ def random_word(file):
 
 
 # This def creates a variable which is actually the random word from previous def, but all the letters are '_' now
-# its commented now, since using this def separately makes the program do the job twice in [def game(…, …)]
+# its commented and moved inside the [def game] now,
+# since using this def separately makes the program do the job twice in [def game(…, …)]
 # def make_word_hidden(word_to_guess):
-    #     i = 0
-    #     underline = ''
-    #     while i < len(word_to_guess):
-    #        print('_ ')
-    #        underline += '_ '
-    #        i += 1
-    #    print('У вас есть 10 попыток, чтобы угадать слово из' + str(len(word_to_guess)) + 'букв')
+#     i = 0
+#     underline = ''
+#     while i < len(word_to_guess):
+#        print('_ ')
+#        underline += '_ '
+#        i += 1
+#    print('У вас есть 10 попыток, чтобы угадать слово из' + str(len(word_to_guess)) + 'букв')
 #    return underline
 
 
 # This def is the gameplay
 def game(word_to_guess):
     attempts = 10
+    list_of_tries = ['ё']
     i = 0
     ii = 0
     iii = 0
@@ -69,6 +67,7 @@ def game(word_to_guess):
     while i < len(word_to_guess):
         underline += '_ '
         i += 1
+    underline = underline[:-1]
     list_underline = underline.split(' ')  # тут была list вместо split
     print(underline)
     print('У вас есть 10 попыток, чтобы угадать слово из ' + str(len(word_to_guess)) + ' букв')
@@ -82,35 +81,40 @@ def game(word_to_guess):
         # 2) if there is only one letter
         # 3) if letter is cyrillic
         if players_guess.isalpha() and len(players_guess) == 1 and bool(re.search('[а-яА-Я]', players_guess)):
-
+            # this tells player if he already tried this letter
+            for i in range(len(list_of_tries)):
+                if players_guess == list_of_tries[i]:
+                    print('Вы уже вводили эту букву')
+            print("Список уже введенных букв: " + str(list_of_tries))
+            list_of_tries.append(players_guess)
             # Player did guess a letter; a letter of the hidden word is revealed.
             # To reveal a letter a string is created from the list.
             for i in range(len(list_of_letters)):
                 if players_guess == list_of_letters[i]:
-                    list_underline[i] = players_guess + ' '
+                    list_underline[i] = players_guess
                     underline = ' '.join(list_underline)
                     ii += 1
 
-                # Player didn't guess the letter
-                # For the sake of optimization its better to reverse the order of elifs,
-                # but here it doesnt really matter
+                    # Player didn't guess the letter
+                    # For the sake of optimization its better to reverse the order of elifs,
+                    # but here it doesn't really matter
             if ii <= iii:
-                    iii = ii
-                    print('Такой буквы в слове нет.')
-                    attempts -= 1
-                    if attempts == 0:
-                        print('Попыток не осталось! Вы проиграли. Хотите сыграть еще раз?')
-                    elif attempts == 1:
-                        print('Осталась 1 попытка')
-                    elif 1 < attempts < 5:
-                        print('Осталось ' + str(attempts) + ' попытки')
-                    elif 5 <= attempts <= 10:
-                        print('Осталось ' + str(attempts) + ' попыток')
+                iii = ii
+                print('Такой буквы в слове нет.')
+                attempts -= 1
+                if attempts == 0:
+                    print('Попыток не осталось! Вы проиграли. Хотите сыграть еще раз?')
+                elif attempts == 1:
+                    print('Осталась 1 попытка')
+                elif 1 < attempts < 5:
+                    print('Осталось ' + str(attempts) + ' попытки')
+                elif 5 <= attempts <= 10:
+                    print('Осталось ' + str(attempts) + ' попыток')
             if underline.split(' ') == list_of_letters:
                 print('Победа! Вы разгадали слово!')
             print(underline)
         else:
-            print("Ошибка ввода. Пожалуйста, введите одну букву русского алфавита.")
+            print("Ошибка ввода. Пожалуйста, введите одну букву русского алфавита. 'Ё' не играет.")
 
 
 game(random_word(open_file_to_list(choose_category())))
