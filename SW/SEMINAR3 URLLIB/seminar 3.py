1c3d513f3a6e0643608a68b70a7cf7db79c90499
toskn <300899@gmail.com>
2018-09-22 11:04:09 +0300

    completed the b) in the 1 task

--- SW/SEMINAR3 URLLIB/seminar 3.py
+++ SW/SEMINAR3 URLLIB/seminar 3.py
@@ -39,4 +39,27 @@ def temp_and_cloud(html):
         print('Облачность: ' + str(final_cloud[0][32:]))
 
 
+def sunrise_sunset(html):
+    # Sunrise part
+    final_sunrise = []
+    regsunrise = re.compile('Восход</span>.*?</dl>', re.DOTALL)
+    sunrise = regsunrise.findall(html)
+    regtag = re.compile('<.*?>', re.DOTALL)
+    for i in sunrise:
+        clean_sunrise = regtag.sub('', i)
+        final_sunrise.append(clean_sunrise)
+        print(str(final_sunrise[0][0:6]) + ' сегодня в ' + str(final_sunrise[0][6:]))
+
+    # Sunset part
+    final_sunset = []
+    regsunset = re.compile('Закат</span>.*?</dl>', re.DOTALL)
+    sunset = regsunset.findall(html)
+    regtag = re.compile('<.*?>', re.DOTALL)
+    for i in sunset:
+        clean_sunset = regtag.sub('', i)
+        final_sunset.append(clean_sunset)
+        print(str(final_sunset[0][0:5]) + ' сегодня в ' + str(final_sunset[0][5:]))
+
+
 temp_and_cloud(downloading_page())
+sunrise_sunset(downloading_page())
