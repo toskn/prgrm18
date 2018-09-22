12cc8ecbb217ff7990f4ac6b9d341b34d4879b84
toskn <300899@gmail.com>
2018-09-22 10:24:17 +0300

    added 2nd task file and some parts of urllib tasks

--- SW/SEMINAR3 URLLIB/seminar 3.py
+++ SW/SEMINAR3 URLLIB/seminar 3.py
@@ -14,20 +14,29 @@ def downloading_page():
         print(e.reason)
 
 
-def temp_and_clouds(html):
+def temp_and_cloud(html):
+    # The part about the temperature
     final_temp = []
-    regtemp = re.compile('<div class="forecast-briefly__name">Сегодня</div>.*?'
-                         '<div class="forecast-briefly__condition">Ясно</div>', re.DOTALL)
+    regtemp = re.compile('Сегодня</div>.*?</a></div>', re.DOTALL)
     temp = regtemp.findall(html)
     regtag = re.compile('<.*?>', re.DOTALL)
-    regword = re.compile(r'[^\x00-\x7F\x80-\xFF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]')
+    regword = re.compile(r'[^\x00-\x7F\x80-\xFF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]')  # nothing except Latin
     regdate = re.compile(r'\d.*?[.]', re.DOTALL)
+    # Actually, i could just slice the important part, but i practiced re.
     for i in temp:
         clean_temp = regtag.sub('', i)
         clean_temp = regword.sub('', clean_temp)
         clean_temp = regdate.sub('', clean_temp)
         final_temp.append(clean_temp)
-        print(final_temp)
+        print('Температура днём: ' + str(final_temp[0][0:4]) + '\n' + 'Температура ночью: ' + str(final_temp[0][4:]))
+
+    # The part about the clouds
+    final_cloud = []
+    cloud = regtemp.findall(html)
+    for i in cloud:
+        clean_cloud = regtag.sub('', i)
+        final_cloud.append(clean_cloud)
+        print('Облачность: ' + str(final_cloud[0][32:]))
 
 
-temp_and_clouds(downloading_page())
+temp_and_cloud(downloading_page())
