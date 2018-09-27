e0b974ea010b062501f92ffc11bfb25b1ae93f56
toskn <300899@gmail.com>
2018-09-27 15:20:44 +0300

    Now the program gets and outputs the name and the description of user repos correctly.

--- HW/TASK2/task2.py
+++ HW/TASK2/task2.py
@@ -8,13 +8,14 @@ import re
 def get_repos():
     user = input('Введите имя пользователя')
     url = 'https://api.github.com/users/%s/repos' % user
-    token = 'b02538ca223cdc7465f127b059d57b35aff50a99'
+    token = 'ccb3a4b775d7e1a38f77fc97ac5496b9b2e16c86'
     headers = {'apikey': 'token %s' % token}
     try:
         request = urllib.request.Request(url, headers=headers)
         response = urllib.request.urlopen(request)
-        text = response.read().decode('utf-8')  # читаем ответ в строку
+        text = response.read().decode('utf-8')
         data = json.loads(text)
+        print('\nВы выбрали пользователя GitHub с ником "' + user + '"\nВот список его репозиториев: \n')
         return data
     except urllib.error.URLError as e:
         print('A problem occurred: ' + str(e.reason))
@@ -30,18 +31,24 @@ def repo_info(data):
     repo_descr = regdescr.findall(str(data))
     repo_name = reglicense.sub('}', str(data))
     repo_name = regname.findall(repo_name)
-    print(repo_descr)
     while i < len(repo_descr):
         repo_descr[i] = regdescr1.sub('', repo_descr[i])
         repo_descr[i] = repo_descr[i][:-9]
+        if repo_descr[i][-1:] == "'":
+            repo_descr[i] = repo_descr[i][:-1]
         i += 1
-    print(repo_descr)
     i = 0
     while i < len(repo_name):
         repo_name[i] = repo_name[i][9:-15]
         i += 1
-    print(repo_name)
-    return repo_name
+    i = 0
+    while i < len(repo_name):
+        if len(repo_name) - 1 != i:
+            print(str(repo_name[i]) + ": " + str(repo_descr[i]) + ",")
+        else:
+            print(str(repo_name[i]) + ": " + str(repo_descr[i]) + ".")
+        i += 1
+    return 0
 
 
 repo_info(get_repos())
